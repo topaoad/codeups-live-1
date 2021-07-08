5c0fc4ecda50310329b6096048b7086f3af327ef
webonds2081 <webonds2081@gmail.com>
2021-07-08 22:34:57 +0900

    0708

---
+++ codeups1/template-section/header/content.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Header Content
+ */
+
+?>
+
+<header class="l-header p-header">
+  <div class="p-header__inner">
+    <div class="p-header__logo">
+      <img class="c-logo" src="<?php echo get_template_directory_uri() ?>/assets/img/common/logo.svg" alt="タイトルロゴ">
+    </div>
+    <div class="p-header__menu c-hamburger">
+      　<span></span>
+      　<span></span>
+      　<span></span>
+    </div>
+  </div>
+</header>
