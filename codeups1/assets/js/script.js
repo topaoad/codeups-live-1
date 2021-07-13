0b7204bb44f2592268e190846c46e326013f043d
webonds2081 <webonds2081@gmail.com>
2021-07-13 23:44:18 +0900

    0713

--- codeups1/assets/js/script.js
+++ codeups1/assets/js/script.js
@@ -1,57 +1,69 @@
 // ローディング判定
 jQuery(function ($) {
-jQuery(window).on("load", function() {
-	jQuery("body").attr("data-loading", "true");
-});
+	jQuery(window).on("load", function() {
+		jQuery("body").attr("data-loading", "true");
+	});
 
-jQuery(function() {
-	// スクロール判定
-	jQuery(window).on("scroll", function() {
-		if (100 < jQuery(this).scrollTop()) {
-			jQuery("body").attr("data-scroll", "true");
+		//ナビバートグル
+	$('.js-hamburger').on('click', function () {
+		if ($('.js-hamburger').hasClass('open')) {
+			$('.js-drawer-menu').fadeOut();
+			$('.js-overlay').fadeOut();
+			$(this).removeClass('open');
 		} else {
-			jQuery("body").attr("data-scroll", "false");
+			$('.js-drawer-menu').fadeIn();
+			$('.js-overlay').fadeIn();
+			$(this).addClass('open');
+
 		}
 	});
 
-	/* ドロワー */
-	jQuery(".js-drawer").on("click", function(e) {
-		e.preventDefault();
-		let targetClass = jQuery(this).attr("data-target");
-		jQuery("." + targetClass).toggleClass("is-checked");
-		return false;
-	});
+		// スクロール判定
+		jQuery(window).on("scroll", function() {
+			if (100 < jQuery(this).scrollTop()) {
+				jQuery("body").attr("data-scroll", "true");
+			} else {
+				jQuery("body").attr("data-scroll", "false");
+			}
+		});
 
-	/* スムーススクロール */
-	jQuery('a[href^="#"]').click(function() {
-		let header = jQuery(".js-header").height();
-		let speed = 300;
-		let id = jQuery(this).attr("href");
-		let target = jQuery("#" == id ? "html" : id);
-		let position = jQuery(target).offset().top - header;
-		if ("fixed" !== jQuery("#header").css("position")) {
-			position = jQuery(target).offset().top;
-		}
-		if (0 > position) {
-			position = 0;
-		}
-		jQuery("html, body").animate(
-			{
-				scrollTop: position
-			},
-			speed
-		);
-		return false;
-	});
+		/* ドロワー */
+		jQuery(".js-drawer").on("click", function(e) {
+			e.preventDefault();
+			let targetClass = jQuery(this).attr("data-target");
+			jQuery("." + targetClass).toggleClass("is-checked");
+			return false;
+		});
 
-	/* 電話リンク */
-	let ua = navigator.userAgent;
-	if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
-		jQuery('a[href^="tel:"]')
-			.css("cursor", "default")
-			.on("click", function(e) {
-				e.preventDefault();
-			});
-	}
-});
+		/* スムーススクロール */
+		jQuery('a[href^="#"]').click(function() {
+			let header = jQuery(".js-header").height();
+			let speed = 300;
+			let id = jQuery(this).attr("href");
+			let target = jQuery("#" == id ? "html" : id);
+			let position = jQuery(target).offset().top - header;
+			if ("fixed" !== jQuery("#header").css("position")) {
+				position = jQuery(target).offset().top;
+			}
+			if (0 > position) {
+				position = 0;
+			}
+			jQuery("html, body").animate(
+				{
+					scrollTop: position
+				},
+				speed
+			);
+			return false;
+		});
+
+		/* 電話リンク */
+		let ua = navigator.userAgent;
+		if (ua.indexOf("iPhone") < 0 && ua.indexOf("Android") < 0) {
+			jQuery('a[href^="tel:"]')
+				.css("cursor", "default")
+				.on("click", function(e) {
+					e.preventDefault();
+				});
+		}
 });
--- codeups1/front-page.php
+++ codeups1/front-page.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
+<div class="l-mv p-mv">
+  <div class="p-mv__img">
+    <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/img_top-sp.jpg" alt="">
+  </div>
+  <div class="p-mv__block">
+    <span class="p-mv__subtitle">デザインで人を笑顔にする会社</span>
+    <span class="p-mv__subtitle--en">DIGSMILE INC.</span>
+    <h2 class="p-mv__title">DESIGN<br>FOR<br>SMILE.</h2>
+  </div>
+
+</div>
 
 
 
